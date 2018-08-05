0aa8f2d3be11a8b94eb4e6ed9af85f29f141b58a
Colby Taperts <colbytaperts@gmail.com>
2018-08-04 22:48:40 -0500

    add in react

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Home from './components/Home/Home';
+
+ReactDOM.render(<Home name="Colby"/>, document.getElementById("root"));
---
+++ client/webpack.config.js
@@ -0,0 +1,23 @@
+const path =require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: [
+          path.resolve(__dirname, "./node_modules"),
+          path.resolve(__dirname, "./build")
+        ]
+      }
+    ]
+  }
+};
+
+module.exports = config;
